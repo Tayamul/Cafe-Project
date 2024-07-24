f709abcb62203b55eb4cc8eeee18b96bbed6a901
Tayamul <rai_tayamul@hotmail.com>
2024-07-24 16:06:18 +0100

    re-order item types

--- src/main/scala/model/Menu.scala
+++ src/main/scala/model/Menu.scala
@@ -1,6 +1,6 @@
 package model
 
-case class Menu(hotDrinks: List[MenuItem], coldDrinks: List[MenuItem],hotFoods: List[MenuItem], coldFoods: List[MenuItem]) {
+case class Menu(hotDrinks: List[MenuItem], coldDrinks: List[MenuItem], hotFoods: List[MenuItem], coldFoods: List[MenuItem]) {
 
   def createMenu: List[MenuItem] = {
     hotDrinks ++ coldDrinks ++ hotFoods ++ coldFoods
@@ -9,10 +9,10 @@ case class Menu(hotDrinks: List[MenuItem], coldDrinks: List[MenuItem],hotFoods: 
 
   def addMenuItem (item: MenuItem): Menu = {
     item.itemType match {
-      case ItemType.ColdDrinks => copy(coldDrinks = coldDrinks :+ item)
       case ItemType.HotDrinks => copy(hotDrinks = hotDrinks :+ item)
-      case ItemType.ColdFoods => copy(coldFoods = coldFoods :+ item)
+      case ItemType.ColdDrinks => copy(coldDrinks = coldDrinks :+ item)
       case ItemType.HotFoods => copy(hotFoods = hotFoods :+ item)
+      case ItemType.ColdFoods => copy(coldFoods = coldFoods :+ item)
     }
 
   }
