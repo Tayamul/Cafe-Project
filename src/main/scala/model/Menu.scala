cab59bedaa294a8af8a99d3b6756beae84c083b6
Jamie Letts <jamieletts2210@gmail.com>
2024-07-25 10:20:11 +0100

    Add removeMenuItem  method

--- src/main/scala/model/Menu.scala
+++ src/main/scala/model/Menu.scala
@@ -7,14 +7,22 @@ case class Menu(hotDrinks: List[MenuItem], coldDrinks: List[MenuItem], hotFoods:
   }
 
 
-  def addMenuItem (item: MenuItem): Menu = {
+  def addMenuItem(item: MenuItem): Menu = {
     item.itemType match {
       case ItemType.HotDrinks => copy(hotDrinks = hotDrinks :+ item)
       case ItemType.ColdDrinks => copy(coldDrinks = coldDrinks :+ item)
       case ItemType.HotFoods => copy(hotFoods = hotFoods :+ item)
       case ItemType.ColdFoods => copy(coldFoods = coldFoods :+ item)
     }
+  }
 
+  def removeMenuItem(item: MenuItem): Menu = {
+    item.itemType match {
+      case ItemType.HotDrinks => copy(hotDrinks = hotDrinks.filterNot(_ == item))
+      case ItemType.ColdDrinks => copy(coldDrinks = coldDrinks.filterNot(_ == item))
+      case ItemType.HotFoods => copy(hotFoods = hotFoods.filterNot(_ == item))
+      case ItemType.ColdFoods => copy(coldFoods = coldFoods.filterNot(_ == item))
+    }
   }
 
 }
