faa9302dcbcb273c8cb7ace3ecfb164dec1ef105
Joe Higgs <joe.higgs@mercator.group>
2024-07-26 12:16:36 +0100

    Update utilsspec tests

--- src/test/scala/utils/UtilsSpec.scala
+++ src/test/scala/utils/UtilsSpec.scala
@@ -1,18 +1,26 @@
 package utils
 
 import java.time.LocalDate
-
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import utils.Utils._
 
 class UtilsSpec extends AnyWordSpec with Matchers {
 
-  //  "onePerDay" should {
-  //    "limit blah blah" when {
-  //      "blahdi blah" in {
-  //
-  //      }
-  //    }
+  "onePerDay" should {
+    "limit the action to once per day" when {
+      "the action was performed today" in {
+        val today = LocalDate.now
+        val lastActionDate = Some(today)
+
+        val (newLastActionDate, actionResult) = onePerDay(lastActionDate) {
+          "action performed"
+        }
+        newLastActionDate shouldBe lastActionDate
+        actionResult shouldBe None
+      }
+    }
+  }
 
 
   //  def onePerDay[T](lastActionDate: Option[LocalDate])(actionCard: => T): (Option[LocalDate], Option[T]) = {
