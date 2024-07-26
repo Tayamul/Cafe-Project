a9c0405db66f1f65c2d5bb36a11ba67aaa8c4b17
Joe Higgs <joe.higgs@mercator.group>
2024-07-26 10:45:05 +0100

    Update discount card tests

--- src/main/scala/services/LoyaltyService.scala
+++ src/main/scala/services/LoyaltyService.scala
@@ -10,31 +10,26 @@ object LoyaltyService {
       case Some(card: DrinksLoyaltyCard) if card.stamps < 10 =>
         val updatedCard = card.copy(stamps = card.stamps + 1)
         customer.copy(loyaltyCard = Some(updatedCard))
-
+      //add one a day
       case _ => customer
     }
   }
 
-  def addStarsToDiscountCard(customer: Customer, menuTotal: Double) = {
+  def addStarsToDiscountCard(customer: Customer, menuTotal: Double): Customer = {
     customer.loyaltyCard match {
       case Some(card: DiscountLoyaltyCard) if menuTotal > 20 =>
         val updatedCard = card.copy(stars = Math.min(card.stars + 1, 8),
           totalSpent = card.totalSpent + menuTotal
         )
         customer.copy(loyaltyCard = Some(updatedCard))
-
+      //add one a day
       case _ => customer
     }
   }
 
   //  def checkLoyaltyEligibility
-  //
-  //  def calculateLoyaltyDiscount
 
-  // Test case 1: Adding a stamp to a DrinksLoyaltyCard with less than 10 stamps
-  // Test case 2: Not modifying the customer if the DrinksLoyaltyCard already has 10 stamps
-  // Test case 3: Not modifying the customer if the customer does not have a DrinksLoyaltyCard
-  // Test case 4: Not modifying the customer if the loyalty card is not a DrinksLoyaltyCard
+  //  def calculateLoyaltyDiscount
 
 
 }
@@ -46,17 +41,16 @@ object LoyaltyService {
  * other card, have purchased from the café less than 5 times or are under 18.
  * Drinks loyalty card – every time they purchase a drink, they receive a stamp (maximum
  * of 1 per day), on the 10th visit their drink is free.
- * o Discount loyalty card - every time their bill comes to more than £20 (once per day) they
- * receive a star. Each star will generate a discount on the total bill (before service charge
- * is applied) of 2%.
- * § The maximum amount of stars is 8, once 8 is reached they will not receive any
- * more stars but the total % discount of 16% will be taken off each bill over £20
- * (not including service charge).
- * § Premium menu items are excluded from this discount.
- * § In order to quality for this discount card, their total spend over a minimum of 5
- * purchases needs to be £150.
- * § E.g. if a customer purchased 4 times, each a minimum of £20, on 4 different
- * days, they will be entitled to a discount of 8% on every purchase (regardless of
- * total price). They can redeem this discount with every purchase, multiple times
- * per day. On their 5th purchase of minimum £20 (after discount scheme), they
- * will receive their 8% discount and a star (taking them to 5). */
+ *
+ *
+ * o Discount loyalty card -
+ * every time their bill comes to more than £20
+ * (once per day) they receive a star.
+ * Each star will generate a discount on the total bill (before service charge is applied) of 2%.
+ * The maximum amount of stars is 8, once 8 is reached they will not receive any more stars but the total % discount of 16% will be taken off each bill over £20 (not including service charge).
+ * Premium menu items are excluded from this discount.
+ * In order to quality for this discount card, their total spend over a minimum of 5 purchases needs to be £150. E.g. if a customer purchased 4 times, each a minimum of £20, on 4 different  days, they will be entitled to a discount of 8% on every purchase (regardless of
+ * total price).
+ * They can redeem this discount with every purchase, multiple times
+ * per day.
+ * On their 5th purchase of minimum £20 (after discount scheme), they will receive their 8% discount and a star (taking them to 5). */
--- src/test/scala/services/LoyaltyServiceSpec.scala
+++ src/test/scala/services/LoyaltyServiceSpec.scala
@@ -15,17 +15,44 @@ class LoyaltyServiceSpec extends AnyWordSpec with Matchers {
         updatedTom.loyaltyCard shouldBe Some(DrinksLoyaltyCard(stamps = 6))
       }
     }
+    "fail to add a stamp" when {
+      "the customer already has 10 stamps" in {
+        val initialCard = DrinksLoyaltyCard(stamps = 10)
+        val tom: Customer = Customer("Tom", 22, Some(initialCard))
+        val updatedTom: Customer = LoyaltyService.addStampToDrinksCard(tom)
+        updatedTom.loyaltyCard shouldBe Some(DrinksLoyaltyCard(stamps = 10))
+      }
+    }
+    "not modify the customer" when {
+      "the customer does not have a DrinksLoyaltyCard" in {
+        val tom: Customer = Customer("Tom", 22, None)
+        val updatedTom: Customer = LoyaltyService.addStampToDrinksCard(tom)
+        updatedTom.loyaltyCard shouldBe None
+      }
+    }
+    "not modify the customer" when {
+      "the loyalty card is not a DrinksLoyaltyCard" in {
+        val initialCard = DiscountLoyaltyCard(stars = 3, totalSpent = 5.00)
+        val tom: Customer = Customer("Tom", 22, Some(initialCard))
+        val updatedTom: Customer = LoyaltyService.addStarsToDiscountCard(tom, 5.00)
+        updatedTom.loyaltyCard shouldBe Some(DiscountLoyaltyCard(3, 5.0))
+      }
+    }
+
 
   }
-  //
-  //  "addStarsToDiscountCard" should {
-  //    "add a star to the customer discount card" when {
-  //      "a purchase is made" in {
-  //        ???
-  //      }
-  //    }
-  //  }
-  //
+
+  "addStarsToDiscountCard" should {
+    "add a star to the customer discount card" when {
+      "a purchase over £20 is made" in {
+        val initialCard = DiscountLoyaltyCard(stars = 3, totalSpent = 20.00)
+        val tom: Customer = Customer("Tom", 22, Some(initialCard))
+        val updatedTom: Customer = LoyaltyService.addStarsToDiscountCard(tom, 25.00)
+        updatedTom.loyaltyCard shouldBe Some(DiscountLoyaltyCard(4, 45.0))
+      }
+    }
+  }
+
   //  "checkLoyaltyEligibility" should {
   //    "confirm a customer is eligible for loyalty perks" in {
   //      ???
